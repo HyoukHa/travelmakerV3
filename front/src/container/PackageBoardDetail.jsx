092bb89047d5c89dfff8a57b7f5c7b084ab7c8c5
rbghk1231@naver.com <rbghk1231@naver.com>
2022-09-15 11:03:51 +0900

    feat: detailmap

--- front/src/container/PackageBoardDetail.jsx
+++ front/src/container/PackageBoardDetail.jsx
@@ -14,17 +14,29 @@ import axios from "axios";
 import { Reply } from ".";
 
 export const PackageBoardDetail = (props) => {
+  const [mapstep, setMapstep] = useState(0);
   const [board, setBoard] = useState({});
+  const [isDetail, setisDetail] = useState(false);
   const { boardId } = useParams();
-
+  console.log("flag1"); //1
+  console.log(board); //2
+  let maxstep = 0;
+  // const selectvalue = (e) => {
+  //   maxstep = e.target.value;
+  //   setMapstep(maxstep);
+  //   console.log(mapstep);
+  // };
   useEffect(() => {
     axios({
       url: `/packageboard/detail/${boardId}`,
       method: "get",
     })
       .then((res) => {
+        console.log("321321");
         console.log(res.data);
-        setBoard(res.data);
+        setBoard({ ...res.data, location: JSON.parse(res.data.location) });
+        setMapstep(maxstep);
+        console.log(mapstep);
       })
       .catch((error) => {
         console.log(error);
@@ -38,10 +50,6 @@ export const PackageBoardDetail = (props) => {
           {board.title}
         </Typography>
         <Divider />
-        <Box>
-          <Typography component="legend"> 평점</Typography>
-          <Rating />
-        </Box>
         <Divider />
         <Typography>작성자 : {board.nickname}</Typography>
         <Divider />
@@ -50,8 +58,34 @@ export const PackageBoardDetail = (props) => {
         </Typography>
         <Divider />
         <Container>
-          <KakaoMap />
+          <select name="steps">
+            {Object.keys(board).length != 0
+              ? board.location.map((item, index) => {
+                  console.log("here");
+                  console.log(item);
+                  maxstep = index;
+                  console.log("max", maxstep);
+                  return (
+                    <option
+                      value={index}
+                      // onChange={selectvalue}
+                    >
+                      {index + 1} 일차
+                    </option>
+                  );
+                })
+              : null}
+          </select>
+          <KakaoMap
+            isDetail={isDetail}
+            board={board.location}
+            maxstep={maxstep}
+          />
         </Container>
+        <Box>
+          <Typography component="legend"> 평점</Typography>
+          <Rating />
+        </Box>
         {props.userId === board.userId ? (
           <Box display="flex" justifyContent="center" justifyItems="center">
             <Button>수정</Button>
